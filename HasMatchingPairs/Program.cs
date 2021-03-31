976dcf97007b258102c415481f913ea22f5ef7e3
Natan <natanrdutra@gmail.com>
2021-03-31 19:10:08 -0400

    Plug and play

---
+++ HasMatchingPairs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HasMatchingPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var content = "Hello World!";
+            var result = HasMatchingPairs(content);
+
+            Console.WriteLine($"Has matching pairs: {result}");
+        }
+
+        static bool HasMatchingPairs(string content)
+        {   
+            var allPossibleChars = new int[256];
+
+            for(var i = 0; i < content.Length; i++)
+            {
+                var charAsInt = (int)content[i];
+                allPossibleChars[charAsInt]++;
+
+                if (allPossibleChars[charAsInt] >= 2){
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
